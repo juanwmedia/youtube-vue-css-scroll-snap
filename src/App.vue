e9489076678971c76d10176e9e339475f3054102
juanwmedia <juan@wmedia.es>
2020-06-13 17:15:50 +0200

    Starting point

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,30 @@
 <template>
   <div id="app">
-
+    <AppSlider/>
   </div>
 </template>
 
 <script>
 
-
+import AppSlider from './components/AppSlider.vue';
 export default {
   name: 'App',
+  components: {
+    AppSlider
+  }
 }
+
 </script>
 
 <style>
+html, body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/AppSlider.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="slider"></div>
+</template>
+
+<script>
+export default {
+  name: "AppSlider",
+  data() {
+    return {
+      slides: [
+        "https://iso.500px.com/wp-content/uploads/2014/10/2048-51.jpg",
+        "https://images2.minutemediacdn.com/image/upload/c_crop,h_1613,w_2400,x_0,y_17/v1572194246/shape/mentalfloss/87226-gettyimages-537121731.jpg?itok=RQxqLlyM",
+        "https://thenypost.files.wordpress.com/2015/10/shutterstock_90194617.jpg?quality=80&strip=all",
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+.slider {
+  height: 100vh;
+  background-color: #ccc;
+}
+</style>
