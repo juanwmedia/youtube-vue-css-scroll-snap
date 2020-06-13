a28ff64ca5b945eaa637580a3f57b1a9a8cb03b5
juanwmedia <juan@wmedia.es>
2020-06-13 18:32:47 +0200

    Slider finished

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,40 @@
 <template>
   <div id="app">
-    <AppSlider/>
+    <AppSlider :slides="slides" />
   </div>
 </template>
 
 <script>
-
-import AppSlider from './components/AppSlider.vue';
+import AppSlider from "./components/AppSlider.vue";
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      slides: [
+        {
+          image: "https://iso.500px.com/wp-content/uploads/2014/10/2048-51.jpg",
+          text: "Black Cats"
+        },
+        {
+          image: "https://images2.minutemediacdn.com/image/upload/c_crop,h_1613,w_2400,x_0,y_17/v1572194246/shape/mentalfloss/87226-gettyimages-537121731.jpg?itok=RQxqLlyM",
+          text: "Are"
+        },
+        {
+          image: "https://thenypost.files.wordpress.com/2015/10/shutterstock_90194617.jpg?quality=80&strip=all",
+          text: "Awesome 😸"
+        }
+      ],
+    };
+  },
   components: {
-    AppSlider
-  }
-}
-
+    AppSlider,
+  },
+};
 </script>
 
 <style>
-html, body {
+html,
+body {
   height: 100%;
   margin: 0;
   padding: 0;
--- src/components/AppSlider.vue
+++ src/components/AppSlider.vue
@@ -1,25 +1,102 @@
 <template>
-  <div class="slider"></div>
+  <div class="slider" ref="slider">
+    <div class="slider__controls">
+      <a
+        @click="moveSlider(n)"
+        href="#"
+        v-for="n in slides.length"
+        :key="n"
+        class="slider__controls__link"
+        >{{ n }}</a
+      >
+    </div>
+    <div
+      v-for="(slide, index) in slides"
+      :key="index"
+      :style="`background-image:url(${slide.image})`"
+      class="slider__content"
+    >
+    {{ slide.text }}
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "AppSlider",
+  mounted() {
+    this.slider = this.$refs.slider;
+    this.height = this.slider.offsetHeight;
+  },
+  props: {
+    slides: {
+      type: Array,
+      required: true,
+    }
+  },
   data() {
     return {
-      slides: [
-        "https://iso.500px.com/wp-content/uploads/2014/10/2048-51.jpg",
-        "https://images2.minutemediacdn.com/image/upload/c_crop,h_1613,w_2400,x_0,y_17/v1572194246/shape/mentalfloss/87226-gettyimages-537121731.jpg?itok=RQxqLlyM",
-        "https://thenypost.files.wordpress.com/2015/10/shutterstock_90194617.jpg?quality=80&strip=all",
-      ],
+      slider: null,
+      height: null,
     };
   },
+  methods: {
+    moveSlider(n) {
+      //n = n === 1 ? 0 : n;
+      n = n-1;
+      this.slider.scrollTo({
+        top: this.height * n,
+        behavior: 'smooth'
+      })
+    }
+  }
 };
 </script>
 
 <style>
 .slider {
+  position: relative;
   height: 100vh;
+  overflow: scroll;
   background-color: #ccc;
+  scroll-snap-type: y mandatory;
+}
+
+.slider__controls {
+  position: fixed;
+  right: .5rem;
+  top: 0;
+  width: 3rem;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+
+.slider__controls__link {
+  width: 3rem;
+  height: 3rem;
+  background-color: rgba(255, 255, 255, .8);
+  color: black;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-decoration: none;
+  font-weight: bold;
+  margin: 1rem 0;
+}
+
+.slider__content {
+  height: 100vh;
+  background-position: center;
+  background-size: cover;
+  scroll-snap-align: center;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: calc(3rem + 4vmin);
 }
 </style>
